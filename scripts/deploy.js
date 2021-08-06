ac2fc7c5894d1585fb868993e746fe41a3d89171
juandeto <juandeto10@gmail.com>
2021-08-06 13:01:47 -0300

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+
+  const Fck = await hre.ethers.getContractFactory("Fck");
+  const fck = await Fck.deploy();
+
+
+  await fck.deployed()
+
+  console.log("Fck Token deployed to: ", fck.address)
+  // console.log("Governance deployed to: ", governance.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
